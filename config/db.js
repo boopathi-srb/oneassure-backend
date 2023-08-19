1153cfa9bfc6e6d59e6404c2e18e96c210d9f44a
Rathina Boopathi Sundarraj <rathinaboopathi.srb@gmail.com>
2023-08-19 12:37:50 +0530

    bug fix in db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGO_UR
+const uri = process.env.MONGO_URI
   // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
